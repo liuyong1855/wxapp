33a67d0a72c4e76363e136214fadce9ed59dab48
rousong <ruosong@7lk.com>
2018-06-30 15:45:47 +0800

    hs

--- hansong/hantest/client/han/han/han.js
+++ hansong/hantest/client/han/han/han.js
@@ -1,12 +1,69 @@
 
-const app = getApp()
+// const app = getApp()
+// Page({
+//     data: {
+//         // userInfo: {},
+//         // logged: false,
+//         // takeSession: false,
+//         // requestResult: ''
+//     }
+
+
+// })
+
+
 Page({
-    data: {
-        // userInfo: {},
-        // logged: false,
-        // takeSession: false,
-        // requestResult: ''
+    data : {
+        list: [
+            {
+              id: 'view',
+              name: '视图容器',
+              open: false,
+              pages: ['view', 'scroll-view', 'swiper']
+            }, {
+              id: 'content',
+              name: '基础内容',
+              open: false,
+              pages: ['text', 'icon', 'progress']
+            }, {
+              id: 'form',
+              name: '表单组件',
+              open: false,
+              pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+            }, {
+              id: 'nav',
+              name: '导航',
+              open: false,
+              pages: ['navigator']
+            }, {
+              id: 'media',
+              name: '媒体组件',
+              open: false,
+              pages: ['image', 'audio', 'video']
+            }, {
+              id: 'map',
+              name: '地图',
+              pages: ['map']
+            }, {
+              id: 'canvas',
+              name: '画布',
+              pages: ['canvas']
+            }
+          ]
+    },
+    kindToggle : function (e) {
+        // console.log(e,1)
+        var id = e.currentTarget.id, list = this.data.list;
+        for(var i = 0,len = list.length; i  < len; i ++){
+            if(list[i].id == id){
+                list[i].open = !list[i].open
+            }
+            else{
+                list[i].open = false;
+            }
+        }
+        this.setData({
+            list : list
+        })
     }
-
-
-})
+}) 
