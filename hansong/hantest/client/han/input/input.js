3a61e2aebc6b59697725b6c19f2a9742172edd2a
hsir1990 <hsir1990@163.com>
2018-12-21 17:41:44 +0800

    hs

--- hansong/hantest/client/han/input/input.js
+++ hansong/hantest/client/han/input/input.js
@@ -1,32 +1,42 @@
 
 Page({
-    data: {
-        items: [
-          {value: 'USA', name: '美国'},
-          {value: 'CHN', name: '中国', checked: 'true'},
-          {value: 'BRA', name: '巴西'},
-          {value: 'JPN', name: '日本'},
-          {value: 'ENG', name: '英国'},
-          {value: 'FRA', name: '法国'}
-        ]
-      },
-    checkboxChange: function(e) {
-        console.log('checkbox发生change事件，携带value值为：', e.detail.value)
-    console.log(e)
-        var items = this.data.items, values = e.detail.value;
-        for (var i = 0, lenI = items.length; i < lenI; ++i) {
-          items[i].checked = false;
-    // debugger;
-          for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-            if(items[i].value == values[j]){
-              items[i].checked = true;
-              break
-            }
-          }
-        }
-    
-        this.setData({
-          items: items
-        })
-      }
-});
+  data : {
+    focus : false,
+    inputValue : ''
+  },
+  bindKeyInput : function (e) {
+    this.setData({
+      inputValue : e.detail.value
+    })
+  },
+  bindReplaceInput : function (e) {
+    console.log('replace' , e.detail.cursor);
+    let value = e.detail.value;
+    // 光标的位置
+    let pos = e.detail.cursor;
+    var left;
+    // debugger
+    if(pos !== -1){
+      //  光标在中间
+      left = e.detail.value.slice(0, pos);
+      // 计算光标的位置
+      pos = left.replace(/11/g, '2').length
+    }
+
+    // 直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
+    //  不明白打了断点就不灵了
+    return{
+      value : value.replace(/11/g, 2),
+      cursor : pos
+    }
+    // 或者直接返回字符串,光标在最后边
+    // return value.replace(/11/g,'2'),
+  },
+  bindHideKeyboard : function (e) {
+    if (e.detail.value === '123') {
+      wx.hideKeyboard()
+    }
+  }
+  
+
+})
